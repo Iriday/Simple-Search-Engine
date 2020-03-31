05db37f45d217249a27ee8ac292c548750969eb1
Igor <GI_Something@protonmail.com>
2020-03-31 18:45:44 +0300

    feat: Add possibility to input 1+ lines with data.Output search results
    Ignore case when comparing strings(data)

--- src/search/SearchEngine.kt
+++ src/search/SearchEngine.kt
@@ -6,23 +6,40 @@ fun main() {
 
 class SearchEngine {
     fun run() {
-        val input = input()
-        val words = input[0].split(Regex("\\s+"))
-        val word = input[1]
+        output("Enter the number of lines with data:")
+        val numOfLines: Int
+        try {
+            numOfLines = input().toInt()
+        } catch (e: NumberFormatException) {
+            output("Error, incorrect input")
+            return
+        }
+        output("Enter all lines with data:")
+        val lines = Array(numOfLines) { input() }
+        output("\nEnter the word:")
+        val word = input()
 
-        output(getWordNumber(word, words)?.toString() ?: "Not found")
+        val searchResults = ArrayList<String>()
+
+        for (line in lines) {
+            val searchResult = getWordNumber(word, line.split(Regex("\\s+")))
+            if (searchResult != -1) searchResults.add(line)
+        }
+
+        if (searchResults.isNotEmpty()) {
+            output("\nfound data:")
+            searchResults.forEach { line -> output(line) }
+        } else output("No data found.")
     }
 
-    private fun input(): Array<String> {
-        return arrayOf(readLine()!!.trim(), readLine()!!.trim())
-    }
+    private fun input(): String = readLine()!!.trim()
 
     private fun output(data: String) = println(data)
 }
 
-fun getWordNumber(word: String, words: List<String>): Int? {
+fun getWordNumber(word: String, words: List<String>): Int { // linear search
     for ((index, w) in words.withIndex()) {
-        if (w == word) return index + 1 // numbers start from 1
+        if (w.equals(word, true)) return index + 1 // numbers start from 1
     }
-    return null
+    return -1
 }
